cf29e701a326887420f28446488adf5d3e372529
vũ xuân thịnh <caocaodua1@gmail.com>
2023-01-28 09:59:58 +0700

    First create DAO directory

---
+++ DAO/DataProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quản_lý_công_ty_du_lịch.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider instance;
+        public static DataProvider Instance
+        {
+            get { if (instance == null) instance = new DataProvider(); return instance; }
+            private set { DataProvider.instance = value; }
+        }
+
+
+        private string connectionSTR = "Data Source=DESKTOP-SRVEET5\\XUANTHINH;Initial Catalog=\"quan ly cong ti du lich\";Integrated Security=True";
+
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable data = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+    }
+}
