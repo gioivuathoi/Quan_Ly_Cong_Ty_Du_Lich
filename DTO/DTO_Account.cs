b9e29e1e5fef59512d6d452df40507dbc37055d4
HDzungx <hdzungx@gmail.com>
2023-02-13 23:24:09 +0700

    load account

---
+++ DTO/DTO_Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quản_lý_công_ty_du_lịch.DTO
+{
+    public class Account
+    {
+        private string userName;
+        private string tenKH;
+        private DateTime ngaySinh;
+        private string cccd;
+        private string email;
+        private string sDT;
+
+
+        public string UserName { get => userName; set => userName = value; }
+        public string TenKH { get => tenKH; set => tenKH = value; }
+        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public string Cccd { get => cccd; set => cccd = value; }
+        public string Email { get => email; set => email = value; }
+        public string SDT { get => sDT; set => sDT = value; }
+
+
+        public Account()
+        { }
+
+        public Account(string userName, string tenKH, DateTime ngaySinh, string cccd, string email, string sDT)
+        {
+            UserName = userName;
+            TenKH = tenKH;
+            NgaySinh = ngaySinh;
+            Cccd = cccd;
+            Email = email;
+            SDT = sDT;
+        }
+    }
+}
