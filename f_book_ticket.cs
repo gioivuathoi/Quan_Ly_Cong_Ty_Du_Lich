ca6ee168bf01607d424478769c2d17b0567e0389
HDzungx <hdzungx@gmail.com>
2023-02-13 20:15:24 +0700

    g

--- f_book_ticket.cs
+++ f_book_ticket.cs
@@ -60,77 +60,7 @@ namespace Quản_lý_công_ty_du_lịch
                 DTGV_TOUR.DataSource = data.Tables[0];
             }
         }
-        //kiểm tra rỗng
-        public bool KTRong(int a, int b)
-        {
-            if (a==0 && b == 0)//rỗng
-            {
-                MessageBox.Show("Chưa chọn số lượng vé để đặt", "Thông Báo");
-                return false;
-            }
-            return true;//đã nhập tất cả
-        }
-        //kiểm mã hóa đơn tránh bị trùng
-        public bool KTmavetrung(int sl)
-        {
-            try
-            {
-                string query = "select count(*) from Ve where ma_ve='" + sl + "'";
-                DataProvider provider = new DataProvider();
-                int k = (int)provider.ExecuteScalar(query);//thực thi câu lệnh trả về 1 số
-                if (k > 0)//có tồn tại mã hóa đơn
-                {
-                    return true;
-                }
-                else//chưa có mã hóa đơn
-                {
-                    return false;
-                }
 
-            }
-            catch
-            {
-                return false;//thất bại
-            }
-        }
-        //tao ma hoa don tu dung
-        public int taomavetudong()
-        {
-            try
-            {
-                string query = "select count(*) from Ve";
-                DataProvider provider = new DataProvider();
-                int k = (int)provider.ExecuteScalar(query);//thực thi câu lệnh trả về 1 số
-                if (KTmavetrung(k))//trùng mã hóa đơn
-                {
-                    return k + 1;
-                }
-                else//không trùng mã hóa đơn
-                {
-                    return k;
-                }
-
-            }
-            catch
-            {
-                return 0;
-            }
-        }
-
-        //Đặt tour
-        public bool BookTicket(DTO_BookTicket tk)
-        {
-            try
-            {
-                string query = "exec BookTicket '" + tk.Mave + "'" + "','" + tk.So_nguoi_lon + "','" +tk.So_tre_em + "','" + tk.Gia_ve + "','" + tk.Ngay_di + "','" + tk.Ngay_dat + "','" + tk.Payment + "'";
-                int result = DataProvider.Instance.ExecuteNonQuery(query);
-                return result > 0;
-            }
-            catch
-            {
-                return false;//thất bại
-            }
-        }
         #endregion
         #region events
         private void button_load_tour_Click(object sender, EventArgs e)
@@ -142,7 +72,7 @@ namespace Quản_lý_công_ty_du_lịch
         private void BT_BOOK_TICKET_Click(object sender, EventArgs e)
         {
             //kiểm tra chưa chọn vé
-            if (KTRong(int.Parse(numericUpDownOver16.Text), int.Parse(numericUpDownUnder16.Text)) == false)//nhập sluong
+            if (DAO.DAO_BookTicket.Instance.KTRong(int.Parse(numericUpDownOver16.Text), int.Parse(numericUpDownUnder16.Text)) == false)//nhập sluong
             {
                 return;
             }
@@ -154,12 +84,12 @@ namespace Quản_lý_công_ty_du_lịch
                 //tạo DTO
 
                 int gia = int.Parse(numericUpDownOver16.Text) * int.Parse(DTGV_TOUR.Rows[1].Cells[2].ToString());
-                DTO_BookTicket tk = new DTO_BookTicket(taomavetudong().ToString(), int.Parse(numericUpDownOver16.Text), int.Parse(numericUpDownUnder16.Text), gia, DateTime.Parse(DTP_ngaydi.Text), DateTime.Now, listBox_paytype.Text);
+                DTO_BookTicket tk = new DTO_BookTicket(DAO.DAO_BookTicket.Instance.taomavetudong().ToString(), int.Parse(numericUpDownOver16.Text), int.Parse(numericUpDownUnder16.Text), gia, DateTime.Parse(DTP_ngaydi.Text), DateTime.Now, listBox_paytype.Text);
                 //đặt tour
-                if (BookTicket(tk))//thành  công
+                if (DAO.DAO_BookTicket.Instance.BookTicket(tk))//thành  công
                 {
                     //hiển thị mã ve của hóa đơn
-                    int k = taomavetudong() - 1;//do khi thêm vào stt sẽ tăng 1
+                    int k = DAO.DAO_BookTicket.Instance.taomavetudong() - 1;//do khi thêm vào stt sẽ tăng 1
                     string mave = k.ToString();
                     MessageBox.Show("Đặt vé thành công. Mã vé của bạn là nội dung chuyển khoản. Vui lòng chuyển khoản đến STK 123 của ngân hàng ABC theo đúng nội dung.", "Thông Báo");
                     TB_PAYTEXT.Text = mave;
